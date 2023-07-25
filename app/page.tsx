18b6d3c47ae4c083cd1e937c357683d96c3a09e8
Your Name <ilyos.sultanov2020@gmail.com>
2023-07-25 14:35:10 +0500

    completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ProjectInterface } from "@/common.types";
 import Categories from "@/components/Categories";
+import LoadMore from "@/components/LoadMore";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
@@ -15,40 +16,61 @@ type ProjectSearch = {
   };
 };
 
-const Home = async () => {
-  const data = (await fetchAllProjects()) as unknown as ProjectSearch;
+type SearchParams = {
+  category?: string;
+  endcursor?: string;
+};
+
+type Props = {
+  searchParams: SearchParams;
+};
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
+
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+  const data = (await fetchAllProjects(
+    category,
+    endcursor
+  )) as unknown as ProjectSearch;
   const projectsToDisplay = data?.projectSearch?.edges || [];
 
   if (projectsToDisplay.length === 0) {
     return (
       <section className="flexStart flex-col paddings">
-        Categories
+        <Categories />
         <p className="no-result-text text-center">
           No projects found, go create some first.
         </p>
       </section>
     );
   }
+  const pagination = data?.projectSearch?.pageInfo;
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <Categories />
 
-  <section className="flex-start flex-col paddings mb-16">
-    <Categories />
-
-    <section className="projects-grid">
-      {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
-        <ProjectCard
-          key={node?.id}
-          id={node?.id}
-          image={node?.image}
-          title={node?.title}
-          name={node?.createdBy?.name}
-          avatarUrl={node?.createdBy?.avatarUrl}
-          userId={node?.createdBy?.id}
-        />
-      ))}
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={node?.id}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy?.name}
+            avatarUrl={node?.createdBy?.avatarUrl}
+            userId={node?.createdBy?.id}
+          />
+        ))}
+      </section>
+      <LoadMore
+        startCursor={pagination.startCursor}
+        endCursor={pagination.endCursor}
+        hasPreviousPage={pagination.hasPreviousPage}
+        hasNextPage={pagination.hasNextPage}
+      />
     </section>
-
-    <h1>LoadMore</h1>
-  </section>;
+  );
 };
 
 export default Home;
